4eaccb58947051575c8e2077ceda9e4a9fedf0e7
Orr Hauptman <or.hau2008@gmail.com>
2025-06-11 14:12:48 +0300

    save in file 2

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import keyboard
 def new_event(event : keyboard.KeyboardEvent) -> None :
     print(f"The key is - {event.name}")
     with open("words.txt", "a") as file:
-        file.write(f"{event.name} " )
+        file.write(f"{event.name}" )
         file.flush()
 
 keyboard.on_release(callback=new_event)
